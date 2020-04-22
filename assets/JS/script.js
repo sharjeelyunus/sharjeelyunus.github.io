721ffb1f72351805ad39480d4803d4e11ac3a52f
SharjeelDev <61178058+SharjeelDev@users.noreply.github.com>
2020-04-23 01:41:26 +0500

    👌 IMPROVE: Nav-Bar

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -3,3 +3,15 @@ jQuery(document).ready(function(){
     jQuery('.desktop-view').toggleClass('expand');
   });
 });
+
+jQuery(document).ready(function(){
+  jQuery('.cross').click(function () {
+    jQuery('.desktop-view').toggleClass('expand');
+  });
+});
+
+function myFunction() {
+  document.getElementById("burger").style.display = "none";
+  document.getElementById("cross").style.display = "block";
+  document.getElementById("cross").style.float = "right";
+}
