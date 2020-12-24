aa78cd72ec0cc49e58c3fd143722db7162be62ac
sharjeelyunus <sharjeel924@gmail.com>
2020-12-24 21:18:18 +0500

    📦 NEW: Landing Page

--- assets/JS/nav-script.js
+++ assets/JS/nav-script.js
@@ -14,3 +14,13 @@ function displayCross() {
   document.getElementById("cross").style.display = "block";
   document.getElementById("cross").style.float = "right";
 }
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-60px";
+  }
+}
