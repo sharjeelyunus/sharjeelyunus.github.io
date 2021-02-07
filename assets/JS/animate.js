a5582b4ce68ba046ce9f370343a717369683f40a
sharjeelyunus <sharjeel924@gmail.com>
2021-02-08 01:21:01 +0500

    👌 IMPROVE: animation

---
+++ assets/JS/animate.js
@@ -0,0 +1,24 @@
+$('.cover').hover(
+    function () { $(this).addClass('animate__animated animate__pulse') },
+    function () { $(this).removeClass('animate__animated animate__pulse') }
+);
+
+$('.my-name').hover(
+    function () { $(this).addClass('animate__animated animate__rubberBand') },
+    function () { $(this).removeClass('animate__animated animate__rubberBand') }
+);
+
+$('.skills').hover(
+    function () { $(this).addClass('animate__animated animate__swing') },
+    function () { $(this).removeClass('animate__animated animate__swing') }
+);
+
+$('.certificates').hover(
+    function () { $(this).addClass('animate__animated animate__headShake') },
+    function () { $(this).removeClass('animate__animated animate__headShake') }
+);
+
+$('.fa').hover(
+    function () { $(this).addClass('animate__animated animate__headShake') },
+    function () { $(this).removeClass('animate__animated animate__headShake') }
+);
