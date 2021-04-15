df6005dec04371b4dc7c2f3066e8b75d149241cc
sharjeelyunus <sharjeel924@gmail.com>
2021-04-16 00:12:46 +0500

    👌 IMPROVE: Certificates -> JSON

--- assets/JS/certificates.js
+++ assets/JS/certificates.js
@@ -1,24 +1,28 @@
-var certificates = [
-    `<i class="fas fa-trophy"></i> Elements of AI: Introduction to AI - <em>University of Helsinki</em>`,
-    `<i class="fas fa-trophy"></i> HTML Essential Training - <em>LinkedIn</em>`,
-    `<i class="fas fa-trophy"></i> Programming Foundations Databases - <em>LinkedIn</em>`,
-    `<i class="fas fa-trophy"></i> Graphic Design - <em>DigiSkills</em>`,
-    `<i class="fas fa-trophy"></i> Digital Marketing - <em>DigiSkills</em>`,
-    `<i class="fas fa-trophy"></i> E-Commerce Managment - <em>DigiSkills</em>`,
-    `<i class="fas fa-trophy"></i> Freelancing - <em>DigiSkills</em>`,
-    `<i class="fas fa-trophy"></i> Succeeding in Web Development Full Stack and Front End - <em>LinkedIn</em>`,
-    `<i class="fas fa-trophy"></i> Programming Foundations Fundamentals - <em>LinkedIn</em>`,
-    `<i class="fas fa-trophy"></i> What is Graphic Design? - <em>LinkedIn</em>`,
-    `<i class="fas fa-trophy"></i> Introduction to Flutter Development - <em>App Brewery</em>`,
-    `<i class="fas fa-trophy"></i>Artificial Intelligence Music Creation Certification - <em>Udemy</em>`,
-    `<i class="fas fa-trophy"></i> The Beginners Guide to Cyber Security - <em>Udemy</em>`,
-    `<i class="fas fa-trophy"></i> HTML and CSS for Beginners - <em>Udemy</em>`,
-    `<i class="fas fa-trophy"></i> Communication Skills - <em>Udemy</em>`,
-    `<i class="fas fa-trophy"></i> Javascript Beginner - <em>Udemy</em>`,
-    `<i class="fas fa-trophy"></i> Java Programming - <em>Udemy</em>`
-];
+let certificates = [];
+fetch('../JS/certificates-list.json')
+    .then(results => results.json())
+    .then(results => {
 
-certificates.forEach(myFunction);
-function myFunction(item) {
-    document.getElementById("certificates").innerHTML += `<li>${item}</li>`;
-}
+        let certificatesArray = ''
+        results.forEach(element => {
+            certificatesArray += `
+            <li><i class="fas fa-trophy"></i> ${element.courseName} - <em>${element.institute}</em></li>`
+        });
+        document.getElementById("certificates").innerHTML = certificatesArray
+    });
+
+let gcpBadges = [];
+fetch('../JS/badges.json')
+    .then(results => results.json())
+    .then(results => {
+
+        let badgeArray = ''
+        results.forEach(element => {
+            badgeArray += `
+                <div class="box-q">
+                    <img src="${element.img}" alt="${element.name}" title="${element.name}"/>
+                    <p>${element.name} <br/> <span>${element.issueDate}</span></p>
+                </div>`
+        });
+        document.getElementById("badge_box").innerHTML = badgeArray
+    });
