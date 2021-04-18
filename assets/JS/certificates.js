574d15395a72a437e1ea3cc3632e4a6a23afae52
sharjeelyunus <sharjeel924@gmail.com>
2021-04-18 13:38:27 +0500

    👌 IMPROVE: uploaded data to firebase

--- assets/JS/certificates.js
+++ assets/JS/certificates.js
@@ -1,13 +1,26 @@
-function addItemsToList(courseName, institute) {
+function addCertificatesToList(courseName, institute) {
     var ul = document.getElementById("certificates");
-    var header = document.createElement("h2");
 
     var _courseName = document.createElement("li");
     var _institute = document.createElement("li");
 
     _courseName.innerHTML = `<i class="fas fa-trophy"></i> ${courseName} - <em>${(_institute.innerHTML = institute)}</em>`;
-    ul.appendChild(header);
     ul.appendChild(_courseName);
+}
+
+function addBadgesList(name, issueDate, img) {
+    var div = document.getElementById("badge_box");
+
+    var _name = document.createElement("div");
+    var _issueDate = document.createElement("div");
+    var _img = document.createElement("div");
+
+    _name.innerHTML = `
+            <div class="box-q">
+                <img src="${_img.innerHTML = img}" alt="${_name.innerHTML = name}" title="${_name.innerHTML = name}"/>
+                <p>${_name.innerHTML = name} <br/> <span>${_issueDate.innerHTML = issueDate}</span></p>
+            </div>`;
+    div.appendChild(_name);
 }
 
 function FetchAllData() {
@@ -18,9 +31,23 @@ function FetchAllData() {
             snapshot.forEach(function (ChildSnapshot) {
                 let courseName = ChildSnapshot.val().courseName;
                 let institute = ChildSnapshot.val().institute;
-                addItemsToList(courseName, institute);
+                addCertificatesToList(courseName, institute);
             });
         });
 }
 
-window.onload(FetchAllData());
+function FetchBadgesData() {
+    firebase
+        .database()
+        .ref("GCP-Badges")
+        .once("value", function (snapshot) {
+            snapshot.forEach(function (ChildSnapshot) {
+                let name = ChildSnapshot.val().name;
+                let issueDate = ChildSnapshot.val().issueDate;
+                let img = ChildSnapshot.val().img;
+                addBadgesList(name, issueDate, img);
+            });
+        });
+}
+
+window.onload(FetchAllData(), FetchBadgesData());
