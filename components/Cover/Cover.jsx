b027fd709df3a566a1c46f4e74846368024f6398
sharjeelyunus <sharjeel924@gmail.com>
2022-03-04 19:55:59 +0500

    📦 NEW: Cover

--- components/Cover/Cover.jsx
+++ components/Cover/Cover.jsx
@@ -1,8 +1,95 @@
+import Image from 'next/image';
 import React from 'react';
-import './cover.module.css';
+import { motion } from 'framer-motion';
+import Link from 'next/link';
+import { BsLinkedin, BsGithub, BsTwitter } from 'react-icons/bs';
+import { images } from '../../constants';
+
+import styles from './cover.module.css';
+
+const scaleVarients = {
+  whileInView: {
+    scale: [0, 1],
+    opacity: [0, 1],
+    transition: {
+      duration: 1,
+      ease: 'easeInOut',
+    },
+  },
+};
 
 const Cover = () => {
-  return <section id='Home'>Cover</section>;
+  return (
+    <section id='Home' className={styles.app__cover}>
+      <motion.div
+        whileInView={{ x: [-100, 0], opacity: [0, 1] }}
+        transition={{ duration: 0.5 }}
+        className={styles.app__cover_info}
+      >
+        <div className={styles.app__cover_badge}>
+          <div className={styles.badge_cmp}>
+            <span>👋</span>
+            <div>
+              <p className={styles.p_text}>Hello, I am</p>
+              <h1 className={styles.head_text}>Sharjeel Yunus</h1>
+            </div>
+          </div>
+          <div className={styles.align_flex}>
+            <div className={styles.tag_cmp}>
+              <p className={styles.p_text}>Fullstack Developer</p>
+              <p className={styles.p_text}>React / NextJS Dev</p>
+            </div>
+            <div className={styles.icons_cmp}>
+              <Link href='https://www.linkedin.com/in/sharjeel-yunus'>
+                <a target='_blank'>
+                  <BsLinkedin />
+                </a>
+              </Link>
+              <Link href='https://github.com/sharjeelyunus'>
+                <a target='_blank'>
+                  <BsGithub />
+                </a>
+              </Link>
+              <Link href='https://twitter.com/sharjeelyunus'>
+                <a target='_blank'>
+                  <BsTwitter />
+                </a>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </motion.div>
+
+      <motion.div
+        whileInView={{ opacity: [0, 1] }}
+        transition={{ duration: 0.5, delayChildren: 0.5 }}
+        className={styles.app__cover_img}
+      >
+        <div className={styles.profile_image}>
+          <Image src={images.ProfilePic} alt='Sharjeel Yunus' />
+        </div>
+        <motion.div
+          whileInView={{ scale: [0, 1] }}
+          transition={{ duration: 1, ease: 'easeInOut' }}
+          className={styles.overlay_circle}
+        >
+          <Image src={images.circle} alt='profile_circle' />
+        </motion.div>
+      </motion.div>
+
+      <motion.div
+        variants={scaleVarients}
+        whileInView={scaleVarients.whileInView}
+        className={styles.app__cover_circles}
+      >
+        {[images.react, images.Nextjs, images.node].map((circle, index) => (
+          <div className={styles.circle_cmp} key={`circle-${index}`}>
+            <Image src={circle} width='80%' height='80%' alt='' />
+          </div>
+        ))}
+      </motion.div>
+    </section>
+  );
 };
 
 export default Cover;
---
+++ constants/images.js
@@ -0,0 +1,33 @@
+import ProfilePic from '../assets/sharjeel.png';
+import Artwork from '../assets/Sharjeel artwork.png';
+import api from '../assets/api.png';
+import circle from '../assets/circle.svg';
+import css from '../assets/css.png';
+import figma from '../assets/figma.png';
+import flutter from '../assets/flutter.png';
+import html from '../assets/html.png';
+import javascript from '../assets/javascript.png';
+import node from '../assets/node.png';
+import python from '../assets/python.png';
+import react from '../assets/react.png';
+import redux from '../assets/redux.png';
+import sass from '../assets/sass.png';
+import Nextjs from '../assets/Nextjs.png';
+
+export default {
+    ProfilePic,
+    Artwork,
+    api,
+    circle,
+    css,
+    figma,
+    flutter,
+    html,
+    javascript,
+    node,
+    python,
+    react,
+    redux,
+    sass,
+    Nextjs
+}
